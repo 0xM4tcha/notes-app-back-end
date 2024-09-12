5db63900f6cc8e1e333a14a418d66dca6c8388a1
arif hidayat <ariefhide94@gmail.com>
2024-09-12 17:54:41 +0700

    collaboration implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -9,6 +9,15 @@ class UsersService {
   constructor() {
     this._pool = new Pool();
   }
+
+  async getUsersByUsername(username) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE username LIKE $1',
+      values: [`%${username}%`],
+    };
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
   
   async addUser({ username, password, fullname }) {
     await this.verifyNewUsername(username);
